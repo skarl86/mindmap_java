f5e0be350ee37a6d4125570f9970fad58b41d431
NK <beohemian@gmail.com>
2014-05-20 12:38:16 +0900

    Add Model Controller Class

---
+++ MindmapApplication/src/controller/ModelController.java
@@ -0,0 +1,16 @@
+/**
+ * @FileName 	: ModelController.java
+ * @Project 	: MindmapApplication
+ * @Date 		: 2014. 5. 13.
+ * @Author 		: NCri
+ */
+package controller;
+
+/**
+ * @Class		: ModelController
+ * @Date 		: 2014. 5. 13.
+ * @Author 		: NCri
+ */
+public class ModelController {
+
+}
